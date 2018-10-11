119bb8236c6ddd1daa27f9f5f785345f9504be02
Mojo Major <info@redroostermobile.com>
2018-10-11 12:08:17 +0200

    homework done, except styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+
+import { DebounceInput } from 'react-debounce-input';
+
 import './App.sass';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.onButtonClick = this.onButtonClick.bind(this);
+    this.onImageChange = this.onImageChange.bind(this);
+    this.onChangeUpperText = this.onChangeUpperText.bind(this);
+    this.onChangeLowerText = this.onChangeLowerText.bind(this);
 
+    const randomMemeIndex = Math.floor(Math.random() * (props.memes.length - 1));
     // initally set state
-    this.state = { currentMemeIndex: 0 };
+    this.state = { 
+      currentMemeIndex: randomMemeIndex,
+      upperText: 'upperText',
+      lowerText: 'lowerText'
+    };
   }
 
   afterRender() {
@@ -36,20 +47,72 @@ class App extends Component {
 
   onButtonClick() {
     // subsequent set state.. beware, this is asynchronous and will cause a new render cycle
-    this.setState({ currentMemeIndex: Math.floor(Math.random() * 10) });
+    this.setState({ currentMemeIndex: Math.floor(Math.random() * (this.props.memes.length - 1)) });
   }
+
+  onImageChange(event) {
+    const newIndex = this.props.memes.map((meme) => this.getMemeUrl(meme)).indexOf(event.target.value);
+    this.setState({
+      currentMemeIndex: newIndex
+    });
+  }
+
+  onChangeUpperText(event) {
+    this.setState({
+      upperText: event.target.value
+    });
+  }
+
+  onChangeLowerText(event) {
+    this.setState({
+      lowerText: event.target.value
+    });
+  }
+  
+  // helpers
+  getMemeUrl = meme => Object.values(meme)[0];
+  getMemeTitle = meme => Object.keys(meme)[0];
 
   render() {
     const { memes } = this.props;
-    const { currentMemeIndex } = this.state;
+    const { currentMemeIndex, upperText, lowerText } = this.state;
+
+    let options = [];
+    let defaultValue = this.getMemeUrl(memes[currentMemeIndex]);
+    // build options
+    memes.forEach((meme,index) => {
+      const url = this.getMemeUrl(meme);
+      const title = this.getMemeTitle(meme);
+      if (index === currentMemeIndex)
+        defaultValue = url;
+      const newOption = <option value={url} key={'option' + index}>{title}</option>
+      options.push(newOption);
+    });
 
     return (
       <div className="workshop-app">
-        <img src={memes[currentMemeIndex]} />
+        <select onChange={this.onImageChange} value={defaultValue}>
+          {options}
+        </select>
+        
+        <DebounceInput
+          minLength={2}
+          debounceTimeout={300}
+          value={upperText} onChange={this.onChangeUpperText}
+        />
+        <DebounceInput
+          minLength={2}
+          debounceTimeout={300}
+          value={lowerText} onChange={this.onChangeLowerText}
+        />
+        
         <button className="whatever" onClick={this.onButtonClick}>
-          Press Me!
+          Random Image
         </button>
-        <p className="title">Homework Title goes here: {currentMemeIndex}</p>
+        <p className="title">{this.getMemeTitle(memes[currentMemeIndex])} {currentMemeIndex}</p>
+        <div className="imageWrapper">
+          <img src={this.getMemeUrl(memes[currentMemeIndex])} alt="" />
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,46 @@ import App from './components/App/App';
 import memes from './lib/memes';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App memes={Object.values(memes)}  />, document.getElementById('root'));
+// HOMEWORK: (feel free to collaborate)
+ // 1.)
+// Render App.js in 'Class Notation', instead of the 'stateless functional component' above
+// just uncomment the code at the bottom. and comment out the one above
+ // 2.)
+// change the structure of memes to an array of objects in alphabetial order
+// try to use modern js (these kind of operations become very important later on)
+// e.g. [{title: 'Retarded Toad Head', image: 'http://example.com/retoadhead.jpg'},{..},...]
+// change the code in App.js accordingly.
+// render the title under the image via jsx
+ // 3.)
+// increase randomness to the length of the new title,image array (array of tuples)
+ // 4.)
+// add 2 input fields to the dom (name them: upperText, lowerText)
+// add functions that get called when you type sth there (onkeypress) (onfocus) ...
+// don't forget to bind 'this' in the constructor, you will need it later
+ // 5.)
+// exchange the standard input fields for the 'react-debounce-input' component (already in package.json)
+// to not get the keypress handler be called for every single keystroke.
+// find the documentation yourself: online, or deeeeeep inside exercises-for-mobile code
+// then make sure the text of the input fields is rendered in jsx
+ // 6.)
+// add a select/option field that lets you select the images by title
+// and fires an event when sth. has changed.
+// remove the randomness of images
+// remove button and handler
+// within the event: update currentMemeIndex in the state to the selected index (use this.setState)
+ // 7.)
+// BONUS: feel free to use foundation to make things more beautiful
+// just import it. It's already in the package.json
+// beware: make sure not to load foundation twice.
+// remember: we are on still inside our rails app,
+// so check layouts/workshop.haml if it's not included already ;-)
+// maybe try to give the image a maximum width and
+// just make them scale down in proportion if they are too big
+
+const newObject = Object.keys(memes).map(key => ({[key]:memes[key]}));
+//console.log(newObject);
+
+ReactDOM.render(<App memes={newObject}  />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
